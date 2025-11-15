2496812a20f2c50b0d5a3273b753e6eaa59fb898
KechhaDE <Kenny01014@gmail.com>
2025-11-16 00:07:37 +0100

    Testing

--- src/main/java/de/kechha/allesgut/AllesGut.java
+++ src/main/java/de/kechha/allesgut/AllesGut.java
@@ -9,6 +9,7 @@ public class AllesGut implements ModInitializer {
 	public static final String MOD_ID = "allesgut";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    // Test
 	@Override
 	public void onInitialize() {
 
